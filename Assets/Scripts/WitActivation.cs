bd8a0e0408e5f3731e04067a709746ff24a9c4c7
adilallo <39313955+adilallo@users.noreply.github.com>
2023-07-14 11:16:53 -0600

    reimport oculus integration

---
+++ Assets/Scripts/WitActivation.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using Oculus.Voice;
+using UnityEngine;
+
+public class WitActivation : MonoBehaviour
+{
+    private AppVoiceExperience _voiceExperience;
+    private void OnValidate()
+    {
+        if (!_voiceExperience) _voiceExperience = GetComponent<AppVoiceExperience>();
+    }
+
+    private void Start()
+    {
+        _voiceExperience = GetComponent<AppVoiceExperience>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UnityEngine.Debug.Log("*** Pressed Space bar ***");
+            ActivateWit();
+        }
+    }
+
+    /// <summary>
+    /// Activates Wit i.e. start listening to the user.
+    /// </summary>
+    public void ActivateWit()
+    {
+        _voiceExperience.Activate();
+    }
+}
